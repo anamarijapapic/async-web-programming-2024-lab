9d84ba7ca6062974b92fbf2c4b7747f4d06dc62b
Anamarija Papić <anamarija.papic@agilo.com>
2024-10-21 19:50:24 +0200

    Add 3_vjezba starter files

---
+++ 3_vjezba/kod/index.js
@@ -0,0 +1,10 @@
+// Ovdje ce te pisat funkcije koje vam trebaju. Ako zelite, mozete razdvojit u par datoteka, ali iz ove datoteke vam mora
+// biti exportana glavna funkcija
+
+async function vasaFunkcija() {
+  console.log('...');
+}
+
+module.exports = {
+  vasaFunkcija,
+};
